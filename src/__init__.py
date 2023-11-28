26f931abcd2eb9065d6178d6a0d6c17afb5fd53b
Fanuel <fanuelabebe@gmail.com>
2023-11-28 23:36:05 +0300

    Moved some methods from notebook to loader and util fixed bugs and loaded some data to loader class and tested output

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+from loader import SlackDataLoader
+import sys
+import os
+
+
+
+a = os.path.split(os.getcwd())[0]
+print(os.listdir(a))
+slack_loader = SlackDataLoader(f'../{os.listdir(a)[0]}/anonymized/')
+channels = slack_loader.get_channels()
+for chanel in channels:
+    print(chanel['name'])
+
--- src/utils.py
+++ src/utils.py
@@ -12,6 +12,23 @@ import seaborn as sns
 from nltk.corpus import stopwords
 
 
+def convert_2_timestamp(column, data):
+    """convert from unix time to readable timestamp
+        args: column: columns that needs to be converted to timestamp
+                data: data that has the specified column
+    """
+    if column in data.columns.values:
+        timestamp_ = []
+        for time_unix in data[column]:
+            if time_unix == 0:
+                timestamp_.append(0)
+            else:
+                a = datetime.datetime.fromtimestamp(float(time_unix))
+                timestamp_.append(a.strftime('%Y-%m-%d %H:%M:%S'))
+        return timestamp_
+    else: 
+        print(f"{column} not in data")
+
 def break_combined_weeks(combined_weeks):
     """
     Breaks combined weeks into separate weeks.
