306bb358221ff9fb753920182142a6124ffdeb42
Fanuel <fanuelabebe@gmail.com>
2023-11-29 17:13:00 +0300

    - Appended all read messages from all channels - Did an EDA Analysis for message count for N users

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,13 @@
-from loader import SlackDataLoader
-import sys
-import os
+# from loader import SlackDataLoader
+# import sys
+# import os
 
 
 
-a = os.path.split(os.getcwd())[0]
-print(os.listdir(a))
-slack_loader = SlackDataLoader(f'../{os.listdir(a)[0]}/anonymized/')
-channels = slack_loader.get_channels()
-for chanel in channels:
-    print(chanel['name'])
+# a = os.path.split(os.getcwd())[0]
+# print(os.listdir(a))
+# slack_loader = SlackDataLoader(f'../{os.listdir(a)[0]}/anonymized/')
+# channels = slack_loader.get_channels()
+# for chanel in channels:
+#     print(chanel['name'])
 
